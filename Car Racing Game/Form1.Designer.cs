87b62dd98b8abe86bd77dd4915a1e61cb2e5b38a
Rafael Gaioti <naomuitokid@gmail.com>
2022-01-04 17:52:22 +0000

    Dynamic car changes, score and award Att

--- Car Racing Game/Form1.Designer.cs
+++ Car Racing Game/Form1.Designer.cs
@@ -30,20 +30,20 @@
         {
             this.components = new System.ComponentModel.Container();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.Carro2 = new System.Windows.Forms.PictureBox();
-            this.Carro1 = new System.Windows.Forms.PictureBox();
+            this.AI2 = new System.Windows.Forms.PictureBox();
+            this.AI1 = new System.Windows.Forms.PictureBox();
             this.player = new System.Windows.Forms.PictureBox();
             this.explosão = new System.Windows.Forms.PictureBox();
             this.award = new System.Windows.Forms.PictureBox();
             this.roadTrack2 = new System.Windows.Forms.PictureBox();
             this.roadTrack1 = new System.Windows.Forms.PictureBox();
             this.btnStart = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
+            this.txtScore = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.gameTimer = new System.Windows.Forms.Timer(this.components);
             this.panel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.Carro2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.Carro1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.AI2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.AI1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.explosão)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.award)).BeginInit();
@@ -54,8 +54,8 @@
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.Black;
-            this.panel1.Controls.Add(this.Carro2);
-            this.panel1.Controls.Add(this.Carro1);
+            this.panel1.Controls.Add(this.AI2);
+            this.panel1.Controls.Add(this.AI1);
             this.panel1.Controls.Add(this.player);
             this.panel1.Controls.Add(this.explosão);
             this.panel1.Controls.Add(this.award);
@@ -66,27 +66,27 @@
             this.panel1.Size = new System.Drawing.Size(475, 519);
             this.panel1.TabIndex = 0;
             // 
-            // Carro2
+            // AI2
             // 
-            this.Carro2.Image = global::Car_Racing_Game.Properties.Resources.carGrey;
-            this.Carro2.Location = new System.Drawing.Point(360, 56);
-            this.Carro2.Name = "Carro2";
-            this.Carro2.Size = new System.Drawing.Size(50, 100);
-            this.Carro2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
-            this.Carro2.TabIndex = 2;
-            this.Carro2.TabStop = false;
-            this.Carro2.Tag = "carRight";
+            this.AI2.Image = global::Car_Racing_Game.Properties.Resources.carGrey;
+            this.AI2.Location = new System.Drawing.Point(360, 56);
+            this.AI2.Name = "AI2";
+            this.AI2.Size = new System.Drawing.Size(50, 100);
+            this.AI2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.AI2.TabIndex = 2;
+            this.AI2.TabStop = false;
+            this.AI2.Tag = "carRight";
             // 
-            // Carro1
+            // AI1
             // 
-            this.Carro1.Image = global::Car_Racing_Game.Properties.Resources.carGreen;
-            this.Carro1.Location = new System.Drawing.Point(58, 90);
-            this.Carro1.Name = "Carro1";
-            this.Carro1.Size = new System.Drawing.Size(50, 101);
-            this.Carro1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
-            this.Carro1.TabIndex = 2;
-            this.Carro1.TabStop = false;
-            this.Carro1.Tag = "carLeft";
+            this.AI1.Image = global::Car_Racing_Game.Properties.Resources.carGreen;
+            this.AI1.Location = new System.Drawing.Point(58, 90);
+            this.AI1.Name = "AI1";
+            this.AI1.Size = new System.Drawing.Size(50, 101);
+            this.AI1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.AI1.TabIndex = 2;
+            this.AI1.TabStop = false;
+            this.AI1.Tag = "carLeft";
             // 
             // player
             // 
@@ -149,15 +149,15 @@
             this.btnStart.Text = "Jogar";
             this.btnStart.UseVisualStyleBackColor = true;
             // 
-            // label1
+            // txtScore
             // 
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(12, 534);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(475, 23);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "Pontuação: 0";
-            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.txtScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtScore.Location = new System.Drawing.Point(12, 534);
+            this.txtScore.Name = "txtScore";
+            this.txtScore.Size = new System.Drawing.Size(475, 23);
+            this.txtScore.TabIndex = 2;
+            this.txtScore.Text = "Pontuação: 0";
+            this.txtScore.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // label2
             // 
@@ -181,7 +181,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(499, 781);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtScore);
             this.Controls.Add(this.btnStart);
             this.Controls.Add(this.panel1);
             this.Name = "Form1";
@@ -191,8 +191,8 @@
             this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.keyisup);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.Carro2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.Carro1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.AI2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.AI1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.explosão)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.award)).EndInit();
@@ -206,10 +206,10 @@
 
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Button btnStart;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label txtScore;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.PictureBox Carro2;
-        private System.Windows.Forms.PictureBox Carro1;
+        private System.Windows.Forms.PictureBox AI2;
+        private System.Windows.Forms.PictureBox AI1;
         private System.Windows.Forms.PictureBox player;
         private System.Windows.Forms.PictureBox explosão;
         private System.Windows.Forms.PictureBox award;
